d18192be8236eac640ad4aa100b80d7ef305009b
RNXMAJOR <oyimaaaron@gmail.com>
2021-04-22 17:09:14 -0500

    restart

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_TBpPdhM3D', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6mbp6tjtqk7kh9m40dr75hkp6e', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '4pu48rhr4tha6s816dpr23jsf9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
