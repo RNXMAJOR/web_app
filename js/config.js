c096de616d6ffbd880b27aa0dda551212964283a
RNXMAJOR <oyimaaaron@gmail.com>
2021-04-22 15:03:16 -0500

    nw

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_TBpPdhM3D', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6mbp6tjtqk7kh9m40dr75hkp6e', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
